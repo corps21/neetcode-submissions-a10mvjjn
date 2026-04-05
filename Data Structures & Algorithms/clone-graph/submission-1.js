cedc66868cb6c6ed6e4a5789092a6fc930e7573b
corps21 <tatya-bicchu-21@proton.me>
2026-04-05 11:13:25 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        const map = new Map()
+
+        function helper(node) {
+            if(!node) return null
+            if(map.has(node)) return map.get(node)
+
+            const newNode = new Node(node.val)
+            map.set(node, newNode)
+
+            for(let i = 0; i < node.neighbors.length; i++) {
+                const neigh = helper(node.neighbors[i])
+                if(neigh) {
+                    newNode.neighbors.push(neigh)
+                }
+            }
+
+            return newNode;
+        }
+
+        return helper(node)
+    }
+}
